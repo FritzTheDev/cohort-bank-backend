405e03bd212566a165b4e7d8c5f754ffc639894c
Frederick Johnson <fritzj96@gmail.com>
2020-01-02 13:58:03 -0800

    added: pk to Branch responses

--- branches/serializers.py
+++ branches/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework.serializers import ModelSerializer, IntegerField
 from .models import Branch
 class BranchSerializer(ModelSerializer):
-  branch_id = IntegerField(write_only=True)
   class Meta:
-    fields = ['branch_id', 'address', 'name']
+    fields = ['id', 'address', 'name']
     model = Branch
