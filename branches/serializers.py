235244597722531492c5de4e812d70cac69bde2c
Frederick Johnson <fritzj96@gmail.com>
2020-01-02 09:26:12 -0800

    added: Branch model to BranchSerializer

--- branches/serializers.py
+++ branches/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
-
+from .models import Branch
 class BranchSerializer(ModelSerializer):
   class Meta:
     fields = ['uid', 'address', 'name']
+    model = Branch
