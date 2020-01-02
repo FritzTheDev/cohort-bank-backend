0d0a6d44ca6bc4f7c6ab193896938acb68f76b29
Frederick Johnson <fritzj96@gmail.com>
2020-01-02 09:24:49 -0800

    updated: branches.urls path to be an empty string

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('branches/', include('branches.urls'))
+    path('', include('branches.urls'))
 ]
