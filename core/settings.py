95536463a97e34ccd559709afe50af27f31c86fc
Frederick Johnson <fritzj96@gmail.com>
2020-01-01 22:54:54 -0800

    added: branches app to settings

--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'branches'
 ]
 
 MIDDLEWARE = [
