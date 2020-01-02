1b822e43684d6534c87e155eee027d75631c6a86
Frederick Johnson <fritzj96@gmail.com>
2020-01-02 09:04:53 -0800

    added: BranchViewSet

--- branches/views.py
+++ branches/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Branch
+from .serializers import BranchSerializer
 # Create your views here.
+
+class BranchViewSet(ModelViewSet):
+  queryset = Branch.objects.all()
+  serializer_class = BranchSerializer
