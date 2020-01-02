751c0a1b6ad47a377d31ff61396215720d60ef70
Frederick Johnson <fritzj96@gmail.com>
2020-01-02 08:44:10 -0800

    added: Branch Model

--- branches/models.py
+++ branches/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db.models import Model, CharField
+from uuid import uuid4
 
 # Create your models here.
+
+class Branch(Model):
+  uid = CharField(max_length=50, primary_key=True, default=uuid4())
+  address = CharField(max_length=255)
+  name = CharField(max_length=255)
