f8ba41e49fbebcd7d937099460a57cc0129b1e43
Frederick Johnson <fritzj96@gmail.com>
2020-01-13 15:46:43 -0800

    added: auth to urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('branches.urls'))
+    path('', include('branches.urls')),
+    path('auth/', include('knox.urls'))
 ]
