a321fb287dfbd9e55e410b3a08f5a9bed75564a4
Frederick Johnson <fritzj96@gmail.com>
2020-01-13 16:09:07 -0800

    added: Login view for token

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('branches.urls')),
+    path('login/', LoginView.as_view()),
     path('auth/', include('knox.urls'))
 ]
