d6311228eefbd9aa8732332a91eee11fa9ac0420
Frederick Johnson <fritzj96@gmail.com>
2020-01-16 14:08:37 -0800

    added: urlconfigs to use api/ prefix

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('branches.urls')),
+    path('api/branches/', include('branches.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair")
 ]
